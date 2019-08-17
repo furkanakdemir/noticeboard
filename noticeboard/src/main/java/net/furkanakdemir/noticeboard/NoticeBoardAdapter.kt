01b149e0c9871eb78d263e664c9184528e3994e5
Furkan Akdemir <frukan@gmail.com>
2019-08-17 15:27:46 +0300

    Make adapter allow generic item type

--- noticeboard/src/main/java/net/furkanakdemir/noticeboard/NoticeBoardAdapter.kt
+++ noticeboard/src/main/java/net/furkanakdemir/noticeboard/NoticeBoardAdapter.kt
@@ -11,7 +11,7 @@ import net.furkanakdemir.noticeboard.util.color.ColorProvider
 
 class NoticeBoardAdapter constructor(
     val colorProvider: ColorProvider
-) : RecyclerView.Adapter<BaseViewHolder<NoticeBoardItem>>() {
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
 
     var releaseList: MutableList<NoticeBoardItem> = mutableListOf()
         set(value) {
@@ -20,7 +20,7 @@ class NoticeBoardAdapter constructor(
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<NoticeBoardItem> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
 
         return when (viewType) {
             VIEW_TYPE_HEADER -> HeaderViewHolder(
@@ -31,30 +31,25 @@ class NoticeBoardAdapter constructor(
                 LayoutInflater.from(parent.context)
                     .inflate(R.layout.list_item_change_log, parent, false)
             )
-            else -> ChangeViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(R.layout.list_item_change_log, parent, false)
-            )
+            else -> throw IllegalArgumentException("Invalid view type $viewType")
         }
     }
 
     override fun getItemCount() = releaseList.size
 
-    override fun onBindViewHolder(holder: BaseViewHolder<NoticeBoardItem>, position: Int) {
-        when (val noticeBoardItem = releaseList[position]) {
-            is NoticeBoardItem.ReleaseHeader -> {
-                holder.bind(noticeBoardItem)
-            }
-            is NoticeBoardItem.ChangeItem -> {
-                holder.bind(noticeBoardItem)
-            }
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        val noticeBoardItem = releaseList[position]
+        when (holder) {
+            is HeaderViewHolder -> holder.bind(noticeBoardItem as NoticeBoardItem.ReleaseHeader)
+            is ChangeViewHolder -> holder.bind(noticeBoardItem as NoticeBoardItem.ChangeItem)
+            else -> throw IllegalArgumentException()
         }
     }
 
     override fun getItemViewType(position: Int): Int {
         return when (releaseList[position]) {
             is NoticeBoardItem.ReleaseHeader -> VIEW_TYPE_HEADER
-            else -> VIEW_TYPE_ITEM
+            is NoticeBoardItem.ChangeItem -> VIEW_TYPE_ITEM
         }
     }
 
@@ -64,31 +59,21 @@ class NoticeBoardAdapter constructor(
     }
 
 
-    inner class HeaderViewHolder(itemView: View) : BaseViewHolder<NoticeBoardItem>(itemView) {
-        override fun bind(item: NoticeBoardItem) {
-
-            val model = item as NoticeBoardItem.ReleaseHeader
-
-            itemView.findViewById<TextView>(R.id.dateTextView).text = model.date
-            itemView.findViewById<TextView>(R.id.versionTextView).text = model.version
+    inner class HeaderViewHolder(itemView: View) : BaseViewHolder<NoticeBoardItem.ReleaseHeader>(itemView) {
+        override fun bind(item: NoticeBoardItem.ReleaseHeader) {
+            itemView.findViewById<TextView>(R.id.dateTextView).text = item.date
+            itemView.findViewById<TextView>(R.id.versionTextView).text = item.version
         }
     }
 
-    inner class ChangeViewHolder(itemView: View) : BaseViewHolder<NoticeBoardItem>(itemView) {
-        override fun bind(item: NoticeBoardItem) {
-            val model = item as NoticeBoardItem.ChangeItem
+    inner class ChangeViewHolder(itemView: View) : BaseViewHolder<NoticeBoardItem.ChangeItem>(itemView) {
+        override fun bind(item: NoticeBoardItem.ChangeItem) {
+            itemView.findViewById<TextView>(R.id.change_description).text = item.description
+            itemView.findViewById<TextView>(R.id.change_type).text = item.type.name
 
-            itemView.findViewById<TextView>(R.id.change_description).text = model.description
-            itemView.findViewById<TextView>(R.id.change_type).text = model.type.name
-
-            val colorId = colorProvider.getChangeTypeBackgroundColor(model.type)
+            val colorId = colorProvider.getChangeTypeBackgroundColor(item.type)
 
             DrawableCompat.setTint(itemView.findViewById<TextView>(R.id.change_type).background, colorId)
-
         }
-
     }
 }
-
-
-
