fddc704098849461c435a5c2c99d84ddfa416f5d
Furkan Akdemir <frukan@gmail.com>
2019-10-17 10:29:50 +0300

    Create color sample activities

--- sample/src/main/java/net/furkanakdemir/noticeboardsample/color/ColorSampleActivity.kt
+++ sample/src/main/java/net/furkanakdemir/noticeboardsample/color/ColorSampleActivity.kt
@@ -1,20 +1,34 @@
 package net.furkanakdemir.noticeboardsample.color
 
-import net.furkanakdemir.noticeboard.Source.Dynamic
 import net.furkanakdemir.noticeboardsample.R
-import net.furkanakdemir.noticeboardsample.base.BaseSampleActivity
-import net.furkanakdemir.noticeboardsample.util.CustomColorProvider
-import net.furkanakdemir.noticeboardsample.util.DataGenerator
+import net.furkanakdemir.noticeboardsample.base.BaseListSampleActivity
+import net.furkanakdemir.noticeboardsample.color.ColorSampleActivity.Color.BACKGROUND
+import net.furkanakdemir.noticeboardsample.color.ColorSampleActivity.Color.CHANGE_TYPE
+import net.furkanakdemir.noticeboardsample.color.ColorSampleActivity.Color.DESCRIPTION
+import net.furkanakdemir.noticeboardsample.color.ColorSampleActivity.Color.TITLE
+import net.furkanakdemir.noticeboardsample.ui.SampleItem
+import net.furkanakdemir.noticeboardsample.ui.SampleItem.Sample
+import net.furkanakdemir.noticeboardsample.util.ext.launch
 
-class ColorSampleActivity : BaseSampleActivity() {
+class ColorSampleActivity : BaseListSampleActivity() {
 
-    override fun getDescription(): Int = R.string.description_json
+    override fun createSamples(): List<SampleItem> = Color.values().map { Sample(it.type) }
 
-    override fun getToolbarTitle(): Int = R.string.title_json
+    override fun getToolbarTitle(): Int = R.string.title_custom_color
 
-    override fun buttonAction() {
-        val customColorProvider = CustomColorProvider(this)
-        val source = Dynamic(DataGenerator.createChanges())
-        pinNoticeBoard(source, customColorProvider)
+    override fun getOnClickListener(): (SampleItem) -> Unit = {
+        when (it.title) {
+            CHANGE_TYPE.type -> launch<ChangeTypeColorActivity>()
+            BACKGROUND.type -> launch<BackgroundColorActivity>()
+            DESCRIPTION.type -> launch<DescriptionColorActivity>()
+            TITLE.type -> launch<TitleColorActivity>()
+        }
+    }
+
+    enum class Color(val type: String) {
+        CHANGE_TYPE("Change Type"),
+        BACKGROUND("Background"),
+        DESCRIPTION("Description"),
+        TITLE("Title")
     }
 }
---
+++ sample/src/main/java/net/furkanakdemir/noticeboardsample/color/TitleColorActivity.kt
@@ -0,0 +1,20 @@
+package net.furkanakdemir.noticeboardsample.color
+
+import net.furkanakdemir.noticeboard.Source.Dynamic
+import net.furkanakdemir.noticeboardsample.R
+import net.furkanakdemir.noticeboardsample.base.BaseSampleActivity
+import net.furkanakdemir.noticeboardsample.util.CustomColorProvider
+import net.furkanakdemir.noticeboardsample.util.DataGenerator
+
+class TitleColorActivity : BaseSampleActivity() {
+
+    override fun getDescription(): Int = R.string.description_custom_color_change_type
+
+    override fun getToolbarTitle(): Int = R.string.title_custom_color_change_type
+
+    override fun buttonAction() {
+        val customColorProvider = CustomColorProvider()
+        val source = Dynamic(DataGenerator.createChanges())
+        pinNoticeBoard(source, customColorProvider)
+    }
+}
